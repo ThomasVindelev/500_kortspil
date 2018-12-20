d0d76d539c7c05e00409cdf3f7d717017678c402
ThomasVindelev <44167383+ThomasVindelev@users.noreply.github.com>
2018-12-20 17:20:04 +0100

    Hallo

--- src/Game.java
+++ src/Game.java
@@ -2,5 +2,11 @@ public class Game {
 
     public void newGame(int[][] player1, int[][] player2, int[][] deck) {
 
+        boolean hasCards = false;
+
+        while (!hasCards) {
+
+        }
+
     }
 }
--- src/Rules.java
+++ src/Rules.java
@@ -1,6 +1,6 @@
 public class Rules {
 
-    public void getPossibilities(int[][] playerHand, int[][] cardsOnTable) {
+    /*public void getPossibilities(int[][] playerHand, int[][] cardsOnTable) {
         for (int i = 0; i < playerHand.length; i++) {
             for (int j = 0; j < playerHand[i].length; j++) {
                 if ((cardsOnTable[i][j] > 0 && cardsOnTable[i][j] < 74) && (playerHand[i][j] - 1 == cardsOnTable[i][j] || playerHand[i][j] + 1 == cardsOnTable[i][j])) {
@@ -12,6 +12,6 @@ public class Rules {
                 } else if ()
             }
         }
-    }
+    } */
 
 }
